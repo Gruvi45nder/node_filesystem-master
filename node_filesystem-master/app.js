24c69d738ad1dd2a00c5a0ae65bf8648b8625f10
Gruvi45nder <gurvinder15634@gmail.com>
2024-08-15 06:06:16 -0700

    my commit

---
+++ node_filesystem-master/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+require('dotenv').config();
+const fs = require('fs').promises;
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', async (req, res) => {
+
+    try{
+
+        const fileName = "example.txt";
+        const content = "This is just example text that was written to file";
+
+        //Writing the content to the file
+        await fs.writeFile(fileName, content, 'utf-8');
+
+
+    }catch(error){
+        console.error('Something went wrong:', error);
+        res.status(500).send('Internet Server Error');
+    }
+
+});
+
+
+app.listen(PORT, () =>{
+    console.log(`Connected on port ${PORT}`);
+});
+
+
